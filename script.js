fd2f4048f06b3b9a173ee2730914dea04fb961f1
Daniel Robertson <31508473+DanTheNoodleMan@users.noreply.github.com>
2023-05-08 12:17:25 +0100

    General style changes across all pages

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//I know this is like the worst system every for a verification as you can literally see the passwords in developer tools but it serves its purpose for my goal.
+//The purpose that it serves is that to have the same login screen to give the illusion of multiple pages and each password leading to a different page, achieving simplicity for a simple app where there is no private credentials shared.
+
 let password = "meeting";
 
 function passCheck(){
